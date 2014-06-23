8f3ec171143cc6fe8bade3658edc167067cdfba9
stla <laurent_step@yahoo.fr>
2014-06-24 00:44:29 +0200

    inference function

--- Tests/fiducial_function00.R
+++ Tests/fiducial_function00.R
@@ -173,3 +173,22 @@ sims <- sample(mu, N, prob=WT, replace=TRUE)
 summary(sims)
 print(quantile(sims,c(2.5,97.5)/100))
 plot(density(mu, weights=WT))
+
+inference <- function(vertex, weight, alpha=0.05){ 
+  out <- rep(NA,4)
+  names(out) <- c("mean","median","low","up")
+  out[1] <- sum(vertex*weight)
+  h <- cbind(vertex,weight)
+  hsort <- gx.sort(h,1)
+  hsum <- cumsum(hsort[,2])
+  ci_u <- min(which(hsum>=1-alpha/2)) #upper confidence bound
+  ci_l <- min(which(hsum>=alpha/2))   #lower confidence bound
+  ci_m <- min(which(hsum>=0.5))
+  out[3] <- hsort[ci_l,1]  #lower bound
+  out[4] <- hsort[ci_u,1] #upper bound
+  out[2] <- hsort[ci_m,1] #estimate
+  out
+}
+
+inference(mu,WT)
+
